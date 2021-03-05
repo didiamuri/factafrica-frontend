d8f0c8a0421521a54eb919400670932187837838
kalume92KIN3 <didiamuri@gmail.com>
2021-03-05 02:34:52 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./views/home/home.module').then(m => m.HomeModule) },
+  { path: 'factchecking-demo', loadChildren: () => import('./views/demo/demo.module').then(m => m.DemoModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'factafrica';
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  ngOnDestroy(): void { }
+
 }
